2f501db840a6d22d50b9634dd4b1dc6de5f1e8bc
Guilherme Calderaro <guilhermecald96@gmail.com>
2020-07-29 00:08:13 -0300

    Adicionado Styled-components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,12 +3,18 @@ import React from "react";
 import "./styles.css";
 import Logo from "../../assets/img/logo.png";
 
+import ButtonLink from "./components/ButtonLink";
+
 export default function Menu() {
   return (
     <nav className="Menu">
       <a href="/">
         <img className="Logo" src={Logo} alt="TECHFLIX Logo" />
       </a>
+
+      <ButtonLink href="/" className="ButtonLink">
+        Novo Vídeo
+      </ButtonLink>
     </nav>
   );
 }
