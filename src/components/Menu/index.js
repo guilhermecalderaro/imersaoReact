d176375740ba75e31372da531d0d627950193af8
Guilherme Calderaro <guilhermecald96@gmail.com>
2020-07-29 22:21:42 -0300

    Adicionado react-router-dom

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./styles.css";
 import Logo from "../../assets/img/logo.png";
@@ -9,11 +10,11 @@ import Button from "../Button";
 export default function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="TECHFLIX Logo" />
-      </a>
+      </Link>
 
-      <Button as="a" href="/" className="ButtonLink">
+      <Button as={Link} to="/cadastro/Video" className="ButtonLink">
         Novo Vídeo
       </Button>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import "./index.css";
-import App from "./App";
+
+import Home from "./pages/Home";
+import CadastroVideo from "./pages/cadastro/Video";
+
+const Pagina404 = () => (
+  <main>
+    <h1>Página 404</h1>
+  </main>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
